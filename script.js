36a4742c02c8647b290745170862416de3ec71e6
Blaze Fugina <blaze.fugina@gmail.com>
2017-10-07 18:30:09 -0500

    Basic functionality is working

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function readyNow() {
   $('#submitFN').on('click', annualSalary);
   $('#submitFN').on('click', createTable);
   $('#submitFN').on('click', totalcostMath);
+  $('#submitFN').on('click', averageMath);
+  $('#submitFN').on('click', monthlyCostAdd);
 }
 var employee = [];
 var clickCount = 0;
@@ -18,7 +20,7 @@ function firstName(){
   clickCount++;
   var $firstName = $('#firstName').val();
   console.log($firstName);
-employee.push($firstName);
+  employee.push($firstName);
 }
 function lastName(){
   var $lastName = $('#lastName').val();
@@ -41,40 +43,24 @@ function annualSalary(){
   employee.push($annualSalary);
 }
 
-// var employeeObj = function(employee){
-//   this.firstname = firstName;
-//   this.lastname = lastName;
-//   this.IDfield = IDField;
-//   this.jobTitle = jobTitle;
-//   this.annualSalary = annualSalary;
-
-// };
 function createTable(){
   for (i=0; i < employee.length; i++)
-var tableSelect = "<tr class=\"dataRows\" ><tr><td>" + employee[0] + "</td><td>" + employee[1] + "</td><td>" + employee[2] + "</td><td>" + employee[3] + "</td><td>" + employee[4] + "</td></tr>";
+  var tableSelect = "<tr class=\"dataRows\" ><tr><td>" + employee[0] + "</td><td>" + employee[1] + "</td><td>" + employee[2] + "</td><td>" + employee[3] + "</td><td>" + employee[4] + "</td></tr>";
   $('table').append(tableSelect);
 }
 
 var totalAverageSalary=0;
 
 function totalcostMath(){
-totalAverageSalary = (totalAverageSalary + parseInt(employee[4])) / clickCount;
-return totalAverageSalary;
+  totalAverageSalary = (totalAverageSalary + parseInt(employee[4])) / clickCount;
+  return totalAverageSalary;
 }
 var monthlySalary =0;
 function averageMath(){
-  monthlySalary = totalAverageSalary / 12;
+  monthlySalary = Math.round(totalAverageSalary / 12);
   return monthlySalary;
 }
-
-// var employee = new employeeObject($firstName, $lastName, $IDField, $JobTitle, $annualSalary);
-
-// function showValues() {
-// var str = $("form").serialize();
-// $("results").text( str );
-//  }
-// console.log(str);
-// $('form').on('submit', function(event){
-//   event.preventDefault();
-// console.log( $( this ).serializeArray() );
-// });
+function monthlyCostAdd(){
+  var monthlyCostVar = "<div id=\"TotalCost\"><p>" + monthlySalary + "</p></div>";
+  $('#TotalCost').html(monthlyCostVar);
+}
