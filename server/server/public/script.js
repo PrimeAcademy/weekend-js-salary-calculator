b2e6118cddb9fcdc1a04f8664f6e54e9e01c4179
Blaze Fugina <blaze.fugina@gmail.com>
2018-01-24 12:20:17 -0600

    Fixed calculations.

--- server/server/public/script.js
+++ server/server/public/script.js
@@ -19,62 +19,62 @@ var employee = [];
 var clickCount = 0;
 
 console.log(clickCount);
-function firstName(){
-  employee.length=0; //employeeVar
+function firstName() {
+  employee.length = 0; //employeeVar
   clickCount++; //click counter to calculate monthly cost
   var $firstName = $('#firstName').val();
   console.log($firstName);
   employee.push($firstName);
 }
-function lastName(){
+function lastName() {
   var $lastName = $('#lastName').val();
   console.log($lastName);
   employee.push($lastName);
 }
-function IDField(){
+function IDField() {
   var $IDField = $('#ID').val();
   console.log($IDField);
   employee.push($IDField);
 }
-function jobTitle(){
+function jobTitle() {
   var $JobTitle = $('#JobTitle').val();
   console.log($JobTitle);
   employee.push($JobTitle);
 }
-function annualSalary(){
+function annualSalary() {
   var $annualSalary = $('#AnnualSalary').val();
-  console.log($annualSalary );
+  console.log($annualSalary);
   employee.push($annualSalary);
 }
 
-function createTable(){
-  for (i=0; i < employee.length; i++)
-  var tableSelect = "<tr id='dataRows'><tr><td>" + employee[0] + "</td><td>" + employee[1] + "</td><td>" + employee[2] + "</td><td>" + employee[3] + "</td><td>" + employee[4] + "</td><td><button id='delete'>delete</button></td></tr>";
+function createTable() {
+  for (i = 0; i < employee.length; i++)
+    var tableSelect = "<tr id='dataRows'><tr><td>" + employee[0] + "</td><td>" + employee[1] + "</td><td>" + employee[2] + "</td><td>" + employee[3] + "</td><td>" + employee[4] + "</td><td><button id='delete'>delete</button></td></tr>";
   $('table').append(tableSelect);
 }
 
-var totalAverageSalary=0;
+var totalAverageSalary = 0;
 
-function totalcostMath(){
-  totalAverageSalary = (totalAverageSalary + parseInt(employee[4])) / clickCount;
+function totalcostMath() {
+  totalAverageSalary = (totalAverageSalary + parseInt(employee[4]));
   return totalAverageSalary;
 }
-var monthlySalary=0;
-function averageMath(){
+var monthlySalary = 0;
+function averageMath() {
   monthlySalary = Math.round(totalAverageSalary / 12);
   return monthlySalary;
 }
-function monthlyCostAdd(){
+function monthlyCostAdd() {
   var monthlyCostVar = "<div id=\"TotalCost\"><p id=\"costCSS\">" + monthlySalary + "</p></div>";
   $('#TotalCost').html(monthlyCostVar);
 }
-function formReset(){
+function formReset() {
   $('#firstName').val("");
   $('#lastName').val("");
   $('#ID').val("");
   $('#JobTitle').val("");
   $('#AnnualSalary').val("");
 }
-function deleteFunction(){
+function deleteFunction() {
   $(this).remove();
 }
