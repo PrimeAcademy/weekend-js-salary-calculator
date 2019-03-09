a0f9cf874111ef6f2631efc43ac9223b92f2d8ee
Juno Vue <Juno.Vue@Hotmail.com>
2019-03-08 19:35:47 -0600

    updates to client.js. functions seem to append on click.

--- client.js
+++ client.js
@@ -1,7 +1,43 @@
 $(document).ready(readyNow);
 
-
 function readyNow () {
+appendEmployees();
 
+
+function addFirstName () {
+    
+    let firstName = $('#firstName').val();
+    console.log(firstName);
+} // end firstName function 
+
+function addLastName () {
+    let lastName = $('#lastName').val();
+        console.log(lastName);
+} // end lastName function 
+
+function addId () {
+    let employeeId = $('#idNumber').val();
+        console.log(employeeId);
+}  // end addId
+function addTitle () {
+    let title = $('#employeeTitle').val();
+        console.log(title);
+} // end addTitle
+function addSalary () {
+    let salary = $('#annualSalary').val();
+        console.log(salary);
+}
+
+// append employee properties on click
+function appendEmployees () {
+    $('#submitButton').on('click', addFirstName);
+    $('#submitButton').on('click', addLastName)
+    $('#submitButton').on('click', addId)
+    $('#submitButton').on('click', addTitle )
+    $('#submitButton').on('click', addSalary)
+} // end appendEmployees
 } // end readyNow
+
+
+
 
