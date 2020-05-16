3b51808873a669687a09af2055f4b002c282ddc4
HenryMarin-Prime <henrymarin@rocketmail.com>
2020-05-16 11:24:01 -0500

    addEmployee function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,4 +4,21 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('In JQuery');
+    $('#btn-submit').on('click', addEmployee);
+}
+
+//Create a function that takes in the information from the employee form 
+//attach the function to the submit click(event), then run the function to add
+function addEmployee(event){
+    console.log('In addEmployee');
+    //since the event triggers a refresh, prevent the default state
+    event.preventDefault();
+   
+    //when the submit button is pushed, all the information will be gathered
+    const firstName = $('#firstName').val();
+    const lastName = $('#lastName').val();
+    const idNumber = $('#idNumber').val();
+    const position = $('#position').val();
+    const annualSalary = $('#annualSalary').val();
+    
 }
