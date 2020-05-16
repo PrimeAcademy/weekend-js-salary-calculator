ffc51a95fd4a397a37d59fc8418a95b2528cbd38
HenryMarin-Prime <henrymarin@rocketmail.com>
2020-05-16 12:09:51 -0500

    delete function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(readyNow);
 function readyNow(){
     console.log('In JQuery');
     $('#btn-submit').on('click', addEmployee);
+    $('#employeeTableOut').on('click', '.delete', deleteRow);
 }
 
 //to hold the information, all employees will be added to an array
@@ -29,10 +30,49 @@ function addEmployee(event){
     employees.push(employee);
     console.log('The employees array is:', employees);
 
+    //append to DOM must be called before clearing values
+    appendToDOM(employee);
+
     //once the array has been pushed we can clear the values in the table
     $('#firstName').val('');
     $('#lastName').val('');
     $('#idNumber').val('');
     $('#position').val('');
     $('#annualSalary').val('');
+}
+
+//once all the employee information can be gathered from the DOM
+//we can then display the info to a table in the DOM
+
+function appendToDOM(object){
+    console.log('In appendToDOM');
+    $('#employeeTableOut').empty();
+       
+    const row = `<tr>
+    <td>${object.firstName}</td>
+    <td>${object.lastName}</td>
+    <td>${object.idNumber}</td>
+    <td>${object.position}</td>
+    <td>${object.annualSalary}</td>
+    <td><button class= "delete" id= "${object.idNumber}">Delete</button></td>
+    </tr>`
+    $('#employeeTableOut').append(row);
+
+    deleteRow();
+}
+
+//Now the the table can be seen in the DOM we can now move to a delete function
+//a delete button was added to the row in order to be able to remove the item as needed
+//create a function that when pushed, the delete function will remove an added row
+
+function deleteRow(){
+    console.log('in deleteRow');
+    $(this).closest('tr').remove();
+
+    let idNumber = $(this).attr('id');
+    for (let i = 0 ; i < employees.length; i++){
+    if(employees[i].idNumber === idNumber){
+      employees.splice(i,1);
+    }
+}
 }
