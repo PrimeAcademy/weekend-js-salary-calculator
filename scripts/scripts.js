5b17cefd89faae2090554ec5a94f29d80e528899
HenryMarin-Prime <henrymarin@rocketmail.com>
2020-05-16 11:29:45 -0500

    employees array, clear values

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,6 +7,9 @@ function readyNow(){
     $('#btn-submit').on('click', addEmployee);
 }
 
+//to hold the information, all employees will be added to an array
+let employees = [];
+
 //Create a function that takes in the information from the employee form 
 //attach the function to the submit click(event), then run the function to add
 function addEmployee(event){
@@ -20,5 +23,18 @@ function addEmployee(event){
     const idNumber = $('#idNumber').val();
     const position = $('#position').val();
     const annualSalary = $('#annualSalary').val();
-    
+
+    //the list will be added as an object and pushed to an array
+    let employee = {firstName, lastName, idNumber, position, annualSalary}
+    employees.push(employee);
+    console.log('The employees array is:', employees);
+
+    //once the array has been pushed we can clear the values in the table
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#idNumber').val('');
+    $('#position').val('');
+    $('#annualSalary').val('');
+
+
 }
