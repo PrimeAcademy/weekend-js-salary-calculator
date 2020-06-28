37a4eef524ebcef9b82eeb1bc427dc41d8f1381e
Ryan M Gregory <ryan@ryanmgregory.com>
2020-06-27 19:10:32 -0500

    uncommented /*# sourceMappingURL=bootstrap.min.css.map */ in BS file to remove error in console & added directions to JS file

---
+++ script/client.js
@@ -0,0 +1,46 @@
+$(document).ready(init);
+
+function init() {
+  console.log('Initialized');
+}
+
+// Create an application that records employee salaries
+// and adds salaries up to report monthly costs.
+
+// Topics Covered
+// JavaScript
+// jQuery - Selectors, append, and event handling
+
+// The application should have an input form that collects:
+// employee
+// first name,
+// last name,
+// ID number,
+// job title,
+// annual salary.
+
+// A 'Submit' button should collect the form information,
+
+// store the information to calculate monthly costs,
+
+// append information to the DOM and
+// clear the input fields.
+// Using the stored information, calculate monthly costs
+//  and append this to the to DOM.
+
+//  If the total monthly cost exceeds $20,000,
+//      add a red background color to the total monthly cost.
+
+// Create a delete button that removes an employee from the DOM.
+
+// ## Stretch Mode
+
+// Add styling or extra functionality that fits with the theme
+// of this assignment.
+
+// Once the employee is deleted, update the total spend on salaries
+// account for this employee's removal. This will require that the
+// logic knows which element was removed.
+
+// You will need to use `.text()` as a getter or look into
+// jQuery's `.data()` function. This is tricky!
