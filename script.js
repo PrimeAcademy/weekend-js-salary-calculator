9aeee704f6964be9baf6e3797d03c28ef334fdd7
Bryce Morries <brycemorries@gmail.com>
2023-04-02 17:11:13 -0500

    finshed the total calculator and the change background if total reached 20000

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ $(document).ready(onReady);
 function onReady () {
     $('.submit-btn').on('click', employeeInput);
     $('.table-body').on('click', '.delete-btn', deleteRow);
+    let total = 0;
 }
-
+let total = 0
 function employeeInput (event) {
 
     event.preventDefault()
@@ -14,6 +15,7 @@ function employeeInput (event) {
     let id = $('#id-input').val();
     let title = $('#title-input').val();
     let annualSalary = $('#annual-input').val();
+    
 
     $('.table-body').append(`
         <tr>
@@ -22,19 +24,30 @@ function employeeInput (event) {
             <td>${id}</td>
             <td>${title}</td>
             <td>${annualSalary}</td>
-            <td>
+            <td class='delete-btn-cell'>
                 <button class="delete-btn">Delete</button>
             </td>
         </tr>
     `);
-
+    $('#total-ann-salary').text(function(){
+        total += Number(annualSalary);
+        return total;
+    });
     $('#firt-input').val('');
     $('#last-input').val('');
     $('#id-input').val('');
     $('#title-input').val('');
     $('#annual-input').val('');
+
+    if (Number(total) > 20000) {
+        $('#footy .footer').css('background-color', 'rgb(229, 154, 154)');
+    }
+
 }
 
 function deleteRow () {
     $(this).parent().parent().remove();
+}
+function makeRed() {
+    
 }
